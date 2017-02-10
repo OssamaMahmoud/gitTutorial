909660d1a7a1a698f2be7ef1e7d71b1727e69fc6
Ossama <omahmou2@uwo.c>
2017-02-10 16:23:00 -0500

    Added course class and related fields

---
+++ src/Course.java
@@ -0,0 +1,13 @@
+/**
+ * Created by om on 10/02/17.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String name, String desc)
+    {
+        cname = name;
+        description = desc;
+    }
+}
